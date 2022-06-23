745826a899238ebecdf7e64f46abe4ba58968408
opeyemi <opeyemi.afolabi45@gmail.com>
2022-06-23 05:57:27 +0100

    login

--- components/Buttons/BtnPrimary.js
+++ components/Buttons/BtnPrimary.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const BtnPrimary = ({ text, loading, handleClick, link = "" }) => {
   return (
     <div className="w-full">
-      <Link href={link}>
+      <Link href={link} passHref>
         <LoadingButton
           disableElevation
           sx={{
--- components/Layouts/LoginLayout.js
+++ components/Layouts/LoginLayout.js
@@ -1,12 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
 
 const LoginLayout = ({children}) => {
   return (
     <>
-      <div className='bg-black h-[100vh] p-[10rem]'>
+      <div className=' p-[10rem] bg-primary h-[80vh]'>
         {children}
       </div>
-      <div className='bg-white h-[10vh]'>footerr</div>
+      <div className='bg-white h-[10vh]'>
+        {/* <div className='flex flex-row justify-center px-[10rem] m-[auto]'>
+          <div className='flex flex-row  text-center align-middle'>
+            <p className='mr-10'>Need Support?</p>
+            <div className='flex flex-col'>
+              <p>help@shouts.com</p>
+              <p>+234 823 8172 949</p>
+            </div>
+          </div>
+          <div className='flex flex-row'>
+            <p>Download Shout</p>
+            <Image src='/images/playstore.png' alt='playstore' width='120px' height='50px' />
+          </div>
+        </div> */}
+      </div>
       
     </>
   )
